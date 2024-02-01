80f5d7ab514a61b8a00489eb1c0742c72c8ccdd5
zeelrupapara <zeelrupapara@gmail.com>
2024-02-01 11:46:33 +0530

    feat: update the issue and pr contribution API

--- api/database/queries/pr.sql
+++ api/database/queries/pr.sql
@@ -124,3 +124,55 @@ WHERE
     AND org.id = ANY(string_to_array($4, ','))
     AND r.id = ANY(string_to_array($5, ','))
 ORDER BY pr.github_updated_at DESC LIMIT $6 OFFSET $7;
+
+-- name: GetUserWisePullRequestContributionByFilters :many
+WITH CoreData AS (
+    SELECT
+        COUNT(DISTINCT pr.id) AS pr_count,
+        DATE(pr.github_updated_at) AS updated_date,
+        coll.login 
+    FROM
+        public.repositories r
+    JOIN
+        public.repository_collaborators rc ON r.id = rc.repo_id
+    JOIN
+        public.organization_collaborators oc ON rc.organization_collaborator_id = oc.id
+    JOIN
+        public.organizations org ON oc.organization_id = org.id
+    LEFT JOIN
+        public.issues i ON rc.id = i.repository_collaborators_id
+    LEFT JOIN
+        public.pull_requests pr ON rc.id = pr.repository_collaborators_id
+    LEFT JOIN
+        public.assignees a ON (i.id = a.issue_id OR pr.id = a.pr_id)
+    LEFT JOIN
+        public.collaborators coll ON a.collaborator_id = coll.id
+    WHERE
+        (pr.github_updated_at BETWEEN $1 AND $2)   
+        AND coll.id = ANY(string_to_array($3, ','))
+        AND org.id = ANY(string_to_array($4, ','))
+        AND r.id = ANY(string_to_array($5, ','))
+    GROUP BY updated_date, coll.login  
+    ORDER BY updated_date DESC
+),
+DateSeries AS (
+    SELECT user_date::date, login
+    FROM (
+        SELECT generate_series((SELECT min(updated_date) - interval '1 day' FROM CoreData), 
+                               (SELECT max(updated_date) + interval '1 day' FROM CoreData), 
+                               interval '1 day') AS user_date
+    ) x
+    CROSS JOIN (
+        SELECT DISTINCT login
+        FROM CoreData
+        WHERE login IS NOT NULL
+    ) y
+)
+SELECT 
+    ds.user_date, 
+    ds.login, 
+    COALESCE(cd.pr_count, 0) AS pr_count 
+FROM 
+    DateSeries ds 
+LEFT JOIN 
+    CoreData cd ON ds.user_date = cd.updated_date AND cd.login = ds.login;
--- api/models/pr.sql.go
+++ api/models/pr.sql.go
@@ -247,6 +247,102 @@ func (q *Queries) GetPullRequestContributionDetailsByFilters(ctx context.Context
 	return items, nil
 }
 
+const getUserWisePullRequestContributionByFilters = `-- name: GetUserWisePullRequestContributionByFilters :many
+WITH CoreData AS (
+    SELECT
+        COUNT(DISTINCT pr.id) AS pr_count,
+        DATE(pr.github_updated_at) AS updated_date,
+        coll.login 
+    FROM
+        public.repositories r
+    JOIN
+        public.repository_collaborators rc ON r.id = rc.repo_id
+    JOIN
+        public.organization_collaborators oc ON rc.organization_collaborator_id = oc.id
+    JOIN
+        public.organizations org ON oc.organization_id = org.id
+    LEFT JOIN
+        public.issues i ON rc.id = i.repository_collaborators_id
+    LEFT JOIN
+        public.pull_requests pr ON rc.id = pr.repository_collaborators_id
+    LEFT JOIN
+        public.assignees a ON (i.id = a.issue_id OR pr.id = a.pr_id)
+    LEFT JOIN
+        public.collaborators coll ON a.collaborator_id = coll.id
+    WHERE
+        (pr.github_updated_at BETWEEN $1 AND $2)   
+        AND coll.id = ANY(string_to_array($3, ','))
+        AND org.id = ANY(string_to_array($4, ','))
+        AND r.id = ANY(string_to_array($5, ','))
+    GROUP BY updated_date, coll.login  
+    ORDER BY updated_date DESC
+),
+DateSeries AS (
+    SELECT user_date::date, login
+    FROM (
+        SELECT generate_series((SELECT min(updated_date) - interval '1 day' FROM CoreData), 
+                               (SELECT max(updated_date) + interval '1 day' FROM CoreData), 
+                               interval '1 day') AS user_date
+    ) x
+    CROSS JOIN (
+        SELECT DISTINCT login
+        FROM CoreData
+        WHERE login IS NOT NULL
+    ) y
+)
+SELECT 
+    ds.user_date, 
+    ds.login, 
+    COALESCE(cd.pr_count, 0) AS pr_count 
+FROM 
+    DateSeries ds 
+LEFT JOIN 
+    CoreData cd ON ds.user_date = cd.updated_date AND cd.login = ds.login
+`
+
+type GetUserWisePullRequestContributionByFiltersParams struct {
+	GithubUpdatedAt   sql.NullTime `json:"github_updated_at"`
+	GithubUpdatedAt_2 sql.NullTime `json:"github_updated_at_2"`
+	StringToArray     string       `json:"string_to_array"`
+	StringToArray_2   string       `json:"string_to_array_2"`
+	StringToArray_3   string       `json:"string_to_array_3"`
+}
+
+type GetUserWisePullRequestContributionByFiltersRow struct {
+	UserDate time.Time      `json:"user_date"`
+	Login    sql.NullString `json:"login"`
+	PrCount  int64          `json:"pr_count"`
+}
+
+func (q *Queries) GetUserWisePullRequestContributionByFilters(ctx context.Context, arg GetUserWisePullRequestContributionByFiltersParams) ([]GetUserWisePullRequestContributionByFiltersRow, error) {
+	rows, err := q.db.QueryContext(ctx, getUserWisePullRequestContributionByFilters,
+		arg.GithubUpdatedAt,
+		arg.GithubUpdatedAt_2,
+		arg.StringToArray,
+		arg.StringToArray_2,
+		arg.StringToArray_3,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetUserWisePullRequestContributionByFiltersRow
+	for rows.Next() {
+		var i GetUserWisePullRequestContributionByFiltersRow
+		if err := rows.Scan(&i.UserDate, &i.Login, &i.PrCount); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const insertPR = `-- name: InsertPR :one
 INSERT INTO
     "pull_requests" (
